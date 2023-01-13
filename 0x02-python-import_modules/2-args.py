34b0040108eeb2a6d3e932fbb5b19dca327c6562
CalebAckom <caleb.ackom@amalitech.com>
2023-01-13 08:25:21 +0000

    Refactoring

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
+    from sys import argv
+    length = len(argv)
+    length = int(length)
+    if length == 1:
+        print(f"{(length - 1):d} arguments.")
+    elif length == 2:
+        print(f"{(length - 1):d} argument:")
+    elif length > 2:
+        print(f"{(length - 1):d} arguments:")
 
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
-    else:
-        print("{} arguments:".format(i))
-
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+    for i in range(1, length):
+        print(f"{i}: {argv[i]}")
